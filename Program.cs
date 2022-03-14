de8baa13a2fcf7421c52b2cb0f022b38209a897b
Praful <prafulrakhade02@gmail.com>
2022-03-14 17:33:17 +0530

    [praful] Added UC-1 Program File

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Game check = new Game();
+            check.UC_1_Player();
+        }
+    }
+}
